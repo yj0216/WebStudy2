37f9b612b7b8cc7f668450eb1541859730b57745
yj0216 <yeojun0216@naver.com>
2022-10-17 20:40:45 +0900

    old

---
+++ 4.js
@@ -0,0 +1,3 @@
+var tutle = document.querySelector("#tutle1");
+        tutle.addEventListener("mouseover",() => tutle.src = "북이.jpg");
+        tutle.addEventListener("mouseout",() => tutle.src = "더지.jfif");
